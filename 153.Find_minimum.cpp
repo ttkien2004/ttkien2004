b7b9844c8fb60d67f9098fb00bae437116bb6556
ttkien2004 <136047387+ttkien2004@users.noreply.github.com>
2023-06-16 12:00:33 +0700

    Update 153.Find_minimum.cpp

--- 153.Find_minimum.cpp
+++ 153.Find_minimum.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int right = nums.size()-1, left = 0, res = nums[0];        
-        while(left <= right){            
-            int mid = left + (right-left)/2;
-            res = min(res, nums[mid]);
+        int left = 0, right = nums.size()-1, res = nums[0];
+        while(left <= right){
+            int mid = (left+right)/2;
+            res = (nums[mid] < res)? nums[mid] : res;
             if(nums[mid] >= nums[left]){
-                if(nums[left] <= nums[right]){
-                    right = mid - 1;
-                }else
-                    left = mid + 1;
-            }else{
-                right = mid-1;                
-            }
+                if(nums[left] <= nums[right]) right = mid - 1;
+                else left = mid + 1;
+            }else right = mid - 1;
         }
         return res;
     }
