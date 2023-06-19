8b017ae54ba8db11d5040828a55b496baf3342bf
ttkien2004 <136047387+ttkien2004@users.noreply.github.com>
2023-06-19 21:23:03 +0700

    Create 27.Remove_Element.cpp

---
+++ 27.Remove_Element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> arr;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val) arr.push_back(nums[i]);
+        }
+        nums = arr;
+        return arr.size();
+    }
+};
