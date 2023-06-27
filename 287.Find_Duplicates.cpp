ee934c693bc8e9c41b625a9f8c62b55d03c268ba
ttkien2004 <136047387+ttkien2004@users.noreply.github.com>
2023-06-27 11:29:21 +0700

    Create 287.Find_Duplicates.cpp

---
+++ 287.Find_Duplicates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;      
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break;
+        }
+        int slow2 = 0;
+        while(true){
+            slow2 = nums[slow2];
+            slow = nums[slow];
+            if(slow2 == slow) break;
+        }
+        return slow;
+    }
+};
